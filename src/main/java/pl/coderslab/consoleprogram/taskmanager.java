6e9988292836556e75fc4f31996a6ded4d0c76fe
AntoniGierlach <agierlach45@gmail.com>
2022-09-09 11:29:34 +0200

    first commit

---
+++ src/main/java/pl/coderslab/consoleprogram/taskmanager.java
@@ -0,0 +1,14 @@
+package pl.coderslab.consoleprogram;
+
+public class taskmanager {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static String[][] tasks(String str) {
+
+
+    }
+}
